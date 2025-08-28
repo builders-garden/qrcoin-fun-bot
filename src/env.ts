baac44152cc2e0ecbccbd7f1852b1e823a03d3f6
PaoloRollo <paolo.rollo1997@gmail.com>
2025-08-28 19:06:01 +0200

    feat: re-add x.

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,10 @@ const envSchema = z.object({
   NEYNAR_API_KEY: z.string(),
   FARCASTER_SIGNER_UUID: z.string(),
   PONDER_RPC_URL_8453: z.string(),
-  X_API_KEY: z.string(),
+  X_APP_KEY: z.string(),
+  X_APP_SECRET: z.string(),
+  TWITTER_ACCESS_TOKEN_KEY: z.string(),
+  TWITTER_ACCESS_TOKEN_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
--- src/lib/x/x-api.ts
+++ src/lib/x/x-api.ts
@@ -1,4 +1,9 @@
 import { TwitterApi } from "twitter-api-v2";
 import { env } from "../../env";
 
-export const twitterClient = new TwitterApi(env.X_API_KEY);
+export const twitterClient = new TwitterApi({
+  appKey: env.X_APP_KEY,
+  appSecret: env.X_APP_SECRET,
+  accessToken: env.TWITTER_ACCESS_TOKEN_KEY,
+  accessSecret: env.TWITTER_ACCESS_TOKEN_SECRET,
+});
