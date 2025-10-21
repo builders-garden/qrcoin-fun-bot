3b65f67702d508e1b14aece91682f9ee321fcc96
PaoloRollo <paolo.rollo1997@gmail.com>
2025-10-21 18:18:32 +0200

    feat: optional twitter env.

--- src/env.ts
+++ src/env.ts
@@ -6,8 +6,8 @@ const envSchema = z.object({
   PONDER_RPC_URL_8453: z.string(),
   X_APP_KEY: z.string(),
   X_APP_SECRET: z.string(),
-  TWITTER_ACCESS_TOKEN_KEY: z.string(),
-  TWITTER_ACCESS_TOKEN_SECRET: z.string(),
+  TWITTER_ACCESS_TOKEN_KEY: z.string().optional(),
+  TWITTER_ACCESS_TOKEN_SECRET: z.string().optional(),
   OPENAI_MODEL: z.string().default("gpt-4o-mini"),
   OPENAI_API_KEY: z.string(),
 });
